be708b0b0fe085156a267d09c36379ff4308d5ef
缘梦 <12183602+yuanmeng6666@user.noreply.gitee.com>
2024-10-26 13:56:05 +0000

    模型工具
    Signed-off-by: 缘梦 <12183602+yuanmeng6666@user.noreply.gitee.com>

---
+++ db/__init__.py
@@ -0,0 +1,17 @@
+from tortoise.queryset import QuerySetSingle
+from .models import Item
+from typing import Optional,List
+def to_dict(model:QuerySetSingle=None,fields_data:Optional[list]=None)->dict:
+
+    model_keys = ['_partial', '_saved_in_db', '_custom_generated_pk', '_await_when_save']
+    if fields_data is not None:
+        model_keys+=fields_data
+    data=model.__dict__
+    for i in model_keys:
+        del data[i]
+    return data
+def to_list_dict(modelLIst:List[QuerySetSingle],fields_data:Optional[list]=None)->List[dict]:
+    data=[]
+    for i in modelLIst:
+        data.append(to_dict(i,fields_data=fields_data))
+    return data
